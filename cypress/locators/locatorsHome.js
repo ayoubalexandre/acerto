1faaf0de2b9449d51e833425761c6fc6f9e1288e
Alexandre Ayoub <alexandreayoub@MacBook-Air-de-Alexandre.local>
2023-08-26 20:53:48 -0300

    acr002-feat:criação do arquivo de locators

---
+++ cypress/locators/locatorsHome.js
@@ -0,0 +1,9 @@
+//Elementos da página home para interação
+
+const locatorsHome = {
+    HOME: {
+        BTN_CADASTRE_SE: "Cadastre-se para fazer entregas"
+    }
+};
+
+export default locatorsHome;
